d24259490c9add0d8d9198df74e6882f5401ae3c
Eunji Kang <etothej@naver.com>
2023-05-15 15:21:56 +0900

    서비스 코드와 라이브러리 코드 분리하기

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,11 +3,19 @@ const HtmlWebpackPlugin = require("html-webpack-plugin"); // HTML 파일을 생�
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js", // 모듈의 의존성 그래프를 만드는 것으로, src/index.js 파일이 기본값으로 설정되며, 지정된 파일에 연관된 모듈과 라이브러리를 포함한 번들을 만든다.
+  entry: {
+    app: "./src/index.js",
+    subPage: "./src/sub/index.js", // 페이지 단위로 번들 파일을 만든다. --> index.html에 별도의 subPage 스크립트가 추가된다.
+  },
+  // 모듈의 의존성 그래프를 만드는 것으로, src/index.js 파일이 기본값으로 설정되며, 지정된 파일에 연관된 모듈과 라이브러리를 포함한 번들을 만든다.
+  // app 으로 설정하면 main.js가 아닌 app.js로 변경된다.
+
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
-  }, // 번들 파일 위치와 이름을 지정한다. 번들링이 시작되면 dist 폴더가 생성되고 번들링이 끝나면 dist 폴더 아래에 번들 파일이 생성된다. 기본값은 './dist/main.js'이다. 절대 경로를 사용(path.resolve)하는 것을 권장한다.
+    filename: "custom-[name].js",
+  },
+  // 번들 파일 위치와 이름을 지정한다. 번들링이 시작되면 dist 폴더가 생성되고 번들링이 끝나면 dist 폴더 아래에 번들 파일이 생성된다. 기본값은 './dist/main.js'이다. 절대 경로를 사용(path.resolve)하는 것을 권장한다.
+
   module: {
     rules: [
       {
@@ -15,9 +23,22 @@ module.exports = {
         use: "css-loader",
       },
     ],
-  }, // module.rules는 사용하려는 로더의 규칙을 정의한다. 로더명은 use속성에 추가하고, test는 해당 로더에 적용하려는 파일 확장자를 정규식형태로 지정한다. css-loader를 적용한다.
+  },
+  // module.rules는 사용하려는 로더의 규칙을 정의한다. 로더명은 use속성에 추가하고, test는 해당 로더에 적용하려는 파일 확장자를 정규식형태로 지정한다. css-loader를 적용한다.
 
   plugins: [new HtmlWebpackPlugin()],
 
-  mode: "development", // 개발, 프로덕션 환경 분리하기
+  mode: "development",
+  // 개발, 프로덕션 환경 분리하기
+
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      name: "vendors",
+    },
+  },
+  // 번들링 되는 모듈 중 일부 모듈을 분리할 때 사용 (ex: 무거운 외부 라이브러리 or 모듈)
+  // splitChunks 공통으로 사용하는 모듈 또는 라이브러를 별도의 chunk로 분리한다.
+  // name을 생성하면 dist 폴더 안에 해당 값으로 파일이 생성된다.
+  // same filename 에러 - output filename 변경 [name].js
 };
